2e05d711713f0ba90bc0c004e9e23497cd484c61
SantiagoAbrigo <162151824+SantiagoAbrigo@users.noreply.github.com>
2024-03-15 16:49:24 -0500

    changees gudyz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
-import { API_URL } from "./config";
-async function getPages() {
-  const res = await fetch(`${API_URL}/pagesdinamics`);
-  if (!res.ok) {
-    throw new Error("Something went wrong");
-  }
-  const { data } = await res.json();
-  return data;
-}
-interface Page {
-  title: string;
-  content: string;
-}
-export default async function Home() {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Gudyz.commm</h1>
+      <h1>Vercel</h1>
     </main>
   );
 }
